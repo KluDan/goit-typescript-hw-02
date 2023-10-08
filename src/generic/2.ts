4fa4f90c11156a2271273cee53291fcf4ca3d555
Danylo Kliushnychenko <danylktv2@gmail.com>
2023-10-08 21:48:57 +0100

    Finished tasks

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,23 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
+function compare(
+  top: Pick<AllType, "name" | "color">,
+  bottom: Pick<AllType, "position" | "weight">
+): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
+const topObject = { name: "Object", color: "Red" };
+const bottomObject = { position: 2, weight: 5 };
 
+const result = compare(topObject, bottomObject);
+console.log(result);
 export {};
